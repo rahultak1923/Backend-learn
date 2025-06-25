0c5cb427c1df2a6c94ef17be1b35db01cc642b53
Rahul Tak <rahultak955@gmail.com>
2025-06-25 22:53:24 +0530

    javascript dsa start

---
+++ javascript/DSA/basic/basic1.js
@@ -0,0 +1,29 @@
+ /*
+console.log(a)
+var a = 8 // variable phle show karke and baad me decalair karenege to error aayegi 
+*/
+
+
+/*
+let a = 12;
+let b = "13";
+let c = 13;
+console.log(a+b) // string + number = string 
+console.log(typeof(a+b))
+console.log("the total sum is "+ a + c) // string + number = string + number = string
+console.log("the toal number is "+( a+c))
+console.log(a+c+" the toal number is ")
+
+*/
+
+/*
+// typecoversion
+console.log("1"+1);
+console.log("1"-1);
+console.log("1"*1);
+console.log("1"/1);
+
+*/
+
+// let age = Number(prompt("enter your age"))
+// console.log(age)

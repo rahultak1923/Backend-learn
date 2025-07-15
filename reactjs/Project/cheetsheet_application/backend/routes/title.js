47f925645a0c2889350e5937b7125c1b0e482b02
Rahul Tak <rahultak955@gmail.com>
2025-07-15 11:34:02 +0530

    update code api is complete

--- reactjs/Project/cheetsheet_application/backend/routes/title.js
+++ reactjs/Project/cheetsheet_application/backend/routes/title.js
@@ -31,6 +31,27 @@ router.delete("/delete/:id", async(req,res)=>{
   }
 })
 
+router.put("/update/:id", async(req,res)=>{
+  try{
 
+    const titleid = req.params.id;
+    const {title, description} = req.body;
+ 
+    if(!titleid){
+      return res.status(400).json({error: "Code ID is required"})
+    }
+
+    const updatetitle = await TitleSchema.findByIdAndUpdate(titleid,{title, description},{new:true});
+
+    if(!updatetitle){
+      return res.status(404).json({error:"Code is not found"})
+    }
+
+    return res.status(200).json({message:"code updated",title:updatetitle})
+  }catch(error){
+    console.log("error updating codes",error);
+    return res.status(500).json({error: "failed to update jewellery"})
+}
+})
 
 module.exports = router

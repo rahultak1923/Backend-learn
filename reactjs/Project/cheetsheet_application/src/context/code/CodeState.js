98076bcfb5793666d9d0c7f14e69b3d85c634497
Rahul Tak <rahultak955@gmail.com>
2025-08-01 01:26:48 +0530

    show data with contact data

--- reactjs/Project/cheetsheet_application/src/context/code/CodeState.js
+++ reactjs/Project/cheetsheet_application/src/context/code/CodeState.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CodeContext from "./codeContext";
 
 const CodeState= (props)=>{
@@ -11,10 +11,20 @@ const notesInitial = [
       "createdAt": "2025-07-28T20:59:17.882Z",
       "updatedAt": "2025-07-28T20:59:17.882Z",
       "__v": 0
-    }
+    },
+    {
+      "_id": "6887e4a5c1e3b620b88fca66",
+      "user": "68850c7e99d14acabffe8d63",
+      "title": "rahul kumar",
+      "description": "console.log('hello rahul is token')",
+      "createdAt": "2025-07-28T20:59:17.882Z",
+      "updatedAt": "2025-07-28T20:59:17.882Z",
+      "__v": 0
+    },
 ]
+const [codes, setCodes] = useState(notesInitial)
     return(
-        <CodeContext.Provider value={{}}>
+        <CodeContext.Provider value={{codes, setCodes}}>
            {props.children}
         </CodeContext.Provider>
     )

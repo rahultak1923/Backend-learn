6bae2beb4c51807cf456288bdddf205f0645b756
Rahul Tak <rahultak955@gmail.com>
2025-07-31 23:36:31 +0530

    add random data to sample

--- reactjs/Project/cheetsheet_application/src/context/code/CodeState.js
+++ reactjs/Project/cheetsheet_application/src/context/code/CodeState.js
@@ -2,7 +2,17 @@ import React from "react";
 import CodeContext from "./codeContext";
 
 const CodeState= (props)=>{
-
+const notesInitial = [
+    {
+      "_id": "6887e4a5c1e3b620b88fca66",
+      "user": "68850c7e99d14acabffe8d63",
+      "title": "rahul",
+      "description": "console.log('hello rahul is token')",
+      "createdAt": "2025-07-28T20:59:17.882Z",
+      "updatedAt": "2025-07-28T20:59:17.882Z",
+      "__v": 0
+    }
+]
     return(
         <CodeContext.Provider value={{}}>
            {props.children}

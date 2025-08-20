7991e635fd84954269c6f923bd8b3ef7b6ab8838
Rahul Tak <rahultak955@gmail.com>
2025-08-20 23:03:29 +0530

    question20-23

---
+++ javascript/DSA/Questions/question21.js
@@ -0,0 +1,8 @@
+console.log(Math.round(11.5)); // 5 se badha hoga to pura kar dega and kam hoga to kam kar dega 
+console.log(Math.ceil(11.6));
+console.log(Math.ceil(11.2));
+console.log(Math.floor(11.366)); // last element . ke baad wale remove kar deta hai 
+console.log(Math.trunc(11.36)); // ye bi . ke baad wale element remove kar deta hai 
+
+console.log(Math.pow(2,5));
+console.log(Math.sqrt(25));
---
+++ javascript/DSA/Questions/question22.js
@@ -0,0 +1,2 @@
+console.log("OTP genrate")
+console.log(Math.floor(Math.random()*9000)+1000);
